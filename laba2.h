dfc62011d3bce9077d8419b2909c60a28324d02a
Gorl1337 <92310796+Gorl1337@users.noreply.github.com>
2021-12-19 17:15:06 +0300

    Create laba2.h

---
+++ laba2.h
@@ -0,0 +1,8 @@
+#ifndef __HEADER_H__
+#define __HEADER_H__
+void Bistro(float* a, int n);
+void Puzirek(float* a, int n);
+void Vstavki(float* a, int n);
+void PrintMas(float* a, int n);
+void clrscr();
+#endif
